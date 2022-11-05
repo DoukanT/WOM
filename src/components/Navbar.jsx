c2a0374089289ea96eb99455cab38f81fd88a552
berfusahin <berfusahin17@hotmail.com>
2022-11-05 21:41:50 +0300

    search

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import { MagnifyingGlassIcon } from '@heroicons/react/24/outline'
 import { useEffect } from 'react';
 import { useState } from 'react'
 import { Link } from 'react-router-dom';
+import "./Navbar.css"
 
 const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(false)
@@ -23,17 +24,23 @@ const Navbar = () => {
       window.removeEventListener('scroll', handleScroll)
     }
   }, [])
+
+  const [searchbar, setSearchbar] = useState(0);
+  const handletab=(e)=>{
+    setSearchbar(e);
+  }
+
   return (
     
-<header className={`${isScrolled && 'bg-[#141414]'}`}>
+    <header className={`${isScrolled && 'bg-[#141414]'}`}>
         <div className='flex items-center space-x-2 md:space-x-10'>
-        <img
+        <Link to="/"><img
           alt="the world of movie"
           src={Logo}
           width='200px'
           height='auto'
           className="cursor-pointer object-contain"
-        />
+        /></Link>
 
         <ul className='hidden space-x-4 md:flex'>
             <li className='headerLink text-white text-base'><Link to="/">Home</Link></li>
@@ -45,17 +52,24 @@ const Navbar = () => {
 
         </div>
 
-    <div className="flex items-center space-x-4 text-sm font-light">
+      <div className="flex items-center space-x-4 text-sm font-light">
+        {/* burası boş. işe yaramıyorsa kaldırılabilir */}
+      </div>
 
 
+      <div className='flex flex-row items-center'>
+          {/* buradaki action kısmını api.name ile bağlamamız lazım galiba */}
+          <form action="" method="GET">
+            <div onLoadStart={()=>handletab(0)} onDoubleClick={()=>handletab(0)} className={searchbar===1 ? "" :"hidden"}>
+              <input autocomplete="on" id="search-bar" type="text" placeholder="Click twice to close"/>
+            </div>
+          </form>
+          <MagnifyingGlassIcon onClick={()=>handletab(1)} className={searchbar===1 ? "hidden" : "h-8 w-8 text-pink-500 cursor-pointer sm:inline"}/> 
 
-    </div>
-      <div>
-        <MagnifyingGlassIcon className=" hidden h-8 w-8 text-pink-500 sm:inline"/> 
-        <Link to="/login"><button className='text-white p-4'>Sign In</button></Link>
-        <Link to="/register"><button className='bg-pink-500 px-6 py-2 rounded cursor-pointer text-white'>
-        Sign Up
-        </button></Link>
+          <Link to="/login"><button className='text-white p-4'>Sign In</button></Link>
+          <Link to="/register"><button className='bg-pink-500 px-6 py-2 rounded cursor-pointer text-white'>
+          Sign Up
+          </button></Link>
       </div>
     </header>
   )
