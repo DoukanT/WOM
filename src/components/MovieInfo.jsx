03ac799d47a3ce0f30be4a4fd56dbd6e80e5f9ec
DoukanT <dogukanturgut@eskisehir.edu.tr>
2022-11-05 17:44:36 +0300

    Button edited.

--- src/components/MovieInfo.jsx
+++ src/components/MovieInfo.jsx
@@ -34,7 +34,7 @@ const Main = (movieID) => {
           <h1 className='text-3xl md:text-5xl font-bold'>{movie?.title}</h1>
           <div className='my-4'>
             <button className='border bg-gray-300 text-black border-gray-300 py-2 px-5'>
-              Play
+              I've already watched this
             </button>
             <button className='border text-white border-gray-300 py-2 px-5 ml-4'>
               Watch Later
--- src/pages/Moviepage.jsx
+++ src/pages/Moviepage.jsx
@@ -1,6 +1,4 @@
-import requests from '../Requests'
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import "./Moviepage.css"
 import { useLocation } from 'react-router-dom';
 import Main from '../components/MovieInfo';
@@ -10,13 +8,10 @@ import Main from '../components/MovieInfo';
 const Moviepage = () => {
   const data = useLocation();
   const movieID= data.state.id;
-  const requestMovie= 'https://api.themoviedb.org/3/movie/${data.state.id}?api_key=${Requests.key}&language=en-US'
-  
 
   return (
     <>
       <div class='pt-20'>
-        
         <Main movieID={movieID}/>
       </div>
     </>
