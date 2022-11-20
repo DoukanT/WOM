20d9872b1b7aed5bd9be18e76681e4498db1de4c
berfusahin <berfusahin17@hotmail.com>
2022-11-20 16:57:47 +0300

    log kısmı

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -26,15 +26,15 @@ const Login = () => {
 
   return (
     <>
-      <div className='w-full h-screen'>
+      <div className='fixed pt-24 w-full h-full'>
         <img
           className='hidden sm:block absolute w-full h-full object-cover'
           src='https://i.pinimg.com/564x/83/bd/33/83bd33c53bdaa867989cb83c83ba5b76.jpg'
           alt='/'
         />
         <div className='bg-black/60 fixed top-0 left-0 w-full h-screen'></div>
-        <div className='fixed w-full px-4 py-24 z-50'>
-          <div className='max-w-[450px] h-[600px] mx-auto bg-black/75 text-white'>
+        <div className='fixed w-full px-4 py-20 z-50'>
+          <div className='max-w-[450px] h-[450px] mx-auto bg-black/75 text-white'>
             <div className='max-w-[320px] mx-auto py-16'>
               <h1 className='text-3xl font-bold'>Sign In</h1>
               {error ? <p className='p-3 bg-red-400 my-2'>{error}</p> : null}
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -23,15 +23,15 @@ const Signup = () => {
 
   return (
     <>
-      <div className='w-full h-screen'>
-        <img
-          className='hidden sm:block absolute w-full h-full object-cover'
-          src='https://i.pinimg.com/564x/83/bd/33/83bd33c53bdaa867989cb83c83ba5b76.jpg'
-          alt='/'
-        />
+      <div className='fixed pt-24 w-full h-full'>
+          <img
+            className='hidden sm:block absolute w-full h-full object-cover'
+            src='https://i.pinimg.com/564x/83/bd/33/83bd33c53bdaa867989cb83c83ba5b76.jpg'
+            alt='/'
+          />
         <div className='bg-black/60 fixed top-0 left-0 w-full h-screen'></div>
-        <div className='fixed w-full px-4 py-24 z-50'>
-          <div className='max-w-[450px] h-[600px] mx-auto bg-black/75 text-white'>
+        <div className='fixed w-full px-4 pt-20 z-50'>
+          <div className='max-w-[450px] h-[450px] mx-auto bg-black/75 text-white'>
             <div className='max-w-[320px] mx-auto py-16'>
               <h1 className='text-3xl font-bold'>Sign Up</h1>
               <form
@@ -39,14 +39,14 @@ const Signup = () => {
                 className='w-full flex flex-col py-4'
               >
                 <input
-                 onChange={(e) => setEmail(e.target.value)}
+                  onChange={(e) => setEmail(e.target.value)}
                   className='p-3 my-2 bg-gray-700 rouded'
                   type='email'
                   placeholder='Email'
                   autoComplete='email'
                 />
                 <input
-                 onChange={(e) => setPassword(e.target.value)}
+                  onChange={(e) => setPassword(e.target.value)}
                   className='p-3 my-2 bg-gray-700 rouded'
                   type='password'
                   placeholder='Password'
