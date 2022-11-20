dbc6cdcd62a05c15deaa5f104163ce4847323173
DoukanT <dogukanturgut@eskisehir.edu.tr>
2022-11-20 15:46:00 +0300

    onclick

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -30,7 +30,7 @@ const Movie = ({ item }) => {
 
     return (
       
-      <div className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2'>
+      <div onClick={() => navigate("/Moviepage", { state: { id: item?.id } })} className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2'>
         <img
           className='w-full h-auto block'
           src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`}
