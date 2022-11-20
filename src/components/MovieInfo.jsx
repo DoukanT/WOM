8daad56e6f58444c47f32ee5fe6e7935a5c1a6f8
berfusahin <berfusahin17@hotmail.com>
2022-11-20 17:23:16 +0300

    img fix

--- src/components/MovieInfo.jsx
+++ src/components/MovieInfo.jsx
@@ -22,11 +22,11 @@ const Main = (movieID) => {
     }
   };
   return (
-    <div className='w-full h-[600px] text-white'>
+    <div className='pt-3 w-full h-auto text-white'>
       <div className='w-full h-full'>
-        <div className='absolute w-full h-[600px] bg-gradient-to-r from-black'></div>
+        <div className='absolute w-full h-[854px] bg-gradient-to-r from-black'></div>
         <img
-          className='w-full h-full object-cover'
+          className='w-full h-100% object-cover'
           src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
           alt={movie?.title}
         />
