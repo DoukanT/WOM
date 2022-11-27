9d17e3905dc2fab535c9687c1483148f746f2e90
ismaillburakk <ismailburakarikan@eskisehir.edu.tr>
2022-11-27 21:04:43 +0300

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -7,13 +7,18 @@ const Main = (movieID2) => {
 
     const [movie, setMovies] = useState([]);
     const requestMovie="https://api.themoviedb.org/3/movie/"+movieID2.movieID2+"?api_key="+requests.key+"&language=en-US"
-
+    const [cast, setCast] = useState([]);
+    const requestCast= "https://api.themoviedb.org/3/movie/"+movieID2.movieID2+"/credits?api_key="+requests.key+"&language=en-US"
   useEffect(() => {
     axios.get(requestMovie).then((response) => {
       setMovies(response.data);
     });
-  }, []);
-
+  }, [requestMovie]);
+  useEffect(() => {
+    axios.get(requestCast).then((response) => {
+      setCast(response.data);
+    });
+  }, [requestCast]);
 
   const truncateString = (str, num) => {
     if (str?.length > num) {
@@ -22,12 +27,16 @@ const Main = (movieID2) => {
       return str;
     }
   };
+  if (!movie?.genres) {
+    return null
+  }
+  console.log(cast?.crew);
   return (
-    <div className='w-full h-[600px] text-white'>
+    <div className='w-full h-[1000px] text-white'>
       <div className='w-full h-full'>
-        <div className='absolute w-full h-[600px] bg-gradient-to-r from-black'></div>
+        <div className='absolute w-full h-[1000px] bg-gradient-to-r from-black'></div>
         <img
-          className='w-full h-[600px] object-cover'
+          className='w-full h-[1000px] object-cover'
           src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
           alt={movie?.title}
   
@@ -42,12 +51,29 @@ const Main = (movieID2) => {
               Watch Later
             </button>
           </div>
+          <div className='flex'>
+            {movie?.genres.map(({ id, name }) => (
+            <p key={id}>{name}</p>
+          ))}
+          </div>
           <p className='text-gray-400 text-sm'>
             Released: {movie?.release_date}
           </p>
+          <p>Runtime: {movie?.runtime}</p>
+          <div className='flex'>
+            Actors:
+            <p>{cast?.cast[0].name}, &nbsp;</p>
+            <p>{cast?.cast[1].name}, &nbsp;</p>
+            <p>{cast?.cast[2].name}</p>
+          </div>
           <p className='w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-gray-200'>
             {truncateString(movie?.overview)}
           </p>
+          <div>
+           
+          </div>
+          <div>
+        </div>
         </div>
       </div>
     </div>
