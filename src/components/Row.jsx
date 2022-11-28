eda3fd56d91e803825cc7614c5d3b2d4013a0ea7
DoukanT <dogukanturgut@eskisehir.edu.tr>
2022-11-28 11:41:06 +0300

    button color

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -27,7 +27,7 @@ const Row = ({ title, fetchURL, rowID }) => {
       <div className='relative flex items-center group'>
         <MdChevronLeft
           onClick={slideLeft}
-          className='bg-black left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block'
+          className='bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block'
           size={40}
         />
         <div
@@ -40,7 +40,7 @@ const Row = ({ title, fetchURL, rowID }) => {
         </div>
         <MdChevronRight
           onClick={slideRight}
-          className='bg-black right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block'
+          className='bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block'
           size={40}
         />
       </div>
