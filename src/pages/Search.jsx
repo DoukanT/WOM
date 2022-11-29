a22e5bce74f2cb742a5c36e97371227ff4d9ec42
berfusahin <berfusahin17@hotmail.com>
2022-11-29 17:51:24 +0300

    json eklemesi

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,15 +1,18 @@
 import React, { useState } from 'react';
 import { MultiSelect } from "react-multi-select-component";
-import {genres} from "./options"
+import {genres, languages, runTime, age} from "./options"
 
 const Search = () => {
   const [selectedGenre, setSelectedGenre] = useState([]);
+  const [selectedLanguage, setSelectedLanguage] = useState([]);
+  const [selectedTime, setSelectedTime] = useState([]);
+  const [selectedAge, setSelectedAge] = useState([]);
   const [minValue, setminValue] = React.useState("");
   const [maxValue, setmaxValue] = React.useState("");
 
 
 const onChangeHandler = event => {
-   setminValue(event.target.value);
+  setminValue(event.target.value);
 };
 const onChangeHandler2 = event => {
   setmaxValue(event.target.value);
@@ -17,7 +20,7 @@ const onChangeHandler2 = event => {
 
   return (
     <div className='pt-20'>
-      <h1 className='text-white'>Select Fruits</h1>
+      <h1 className='text-white'>Select Genre(s)</h1>
       <MultiSelect
         hasSelectAll={false}
         options={genres}
@@ -25,19 +28,45 @@ const onChangeHandler2 = event => {
         onChange={setSelectedGenre}
         labelledBy="Select Genre"
       />
-      <h1 className=' text-white'>Select Min Year</h1>
-      <input
-        type="text"
-        name="name"
-        onChange={onChangeHandler}
-        value={minValue}
+      <h1 className='text-white'>Select Language(s)</h1>
+      <MultiSelect
+        hasSelectAll={true}
+        options={languages}
+        value={selectedLanguage}
+        onChange={setSelectedLanguage}
+        labelledBy="Select Language"
       />
-      <h1 className=' text-white'>Select Max Year</h1>
-      <input
-        type="text"
-        name="name"
-        onChange={onChangeHandler2}
-        value={maxValue}
+      <h1 className='text-white'>Select Run Time</h1>
+      <MultiSelect
+        hasSelectAll={true}
+        options={runTime}
+        value={selectedTime}
+        onChange={setSelectedTime}
+        labelledBy="Select Run Time"
+      />
+      <div className='flex flex-row'>
+        <h1 className='text-white'>Select Min Year</h1>
+        <input
+          type="text"
+          name="name"
+          onChange={onChangeHandler}
+          value={minValue}
+        />
+        <h1 className='text-white'>Select Max Year</h1>
+        <input
+          type="text"
+          name="name"
+          onChange={onChangeHandler2}
+          value={maxValue}
+        />
+      </div>
+      <h1 className='text-white'>Select Age Filter</h1>
+      <MultiSelect
+        hasSelectAll={false}
+        options={age}
+        value={selectedAge}
+        onChange={setSelectedAge}
+        labelledBy="Select Age"
       />
     </div>
   );
--- src/pages/options.js
+++ src/pages/options.js
@@ -19,3 +19,46 @@ export const genres = [
     { label: "Western", value: "37"},
 
 ]
+
+export const languages = [
+    { label: "Arabic", value: "ar" },
+    { label: "Chinese", value: "zh" },
+    { label: "Czech", value: "cs" },
+    { label: "Danish", value: "da" },
+    { label: "Dutch", value: "nl" },
+    { label: "English", value: "en" },
+    { label: "Finnish", value: "fi" },
+    { label: "French", value: "fr" },
+    { label: "German", value: "de" },
+    { label: "Greek", value: "el" },
+    { label: "Hebrew", value: "he" },
+    { label: "Hindi", value: "hi" },
+    { label: "Hungarian", value: "hu" },
+    { label: "Icelandic", value: "is" },
+    { label: "Indonesian", value: "id" },
+    { label: "Italian", value: "it" },
+    { label: "Japanese", value: "ja" },
+    { label: "Korean", value: "ko" },
+    { label: "Norwegian", value: "no" },
+    { label: "Polish", value: "pl" },
+    { label: "Porteguese", value: "pt" },
+    { label: "Romanian", value: "ro" },
+    { label: "Russian", value: "ru" },
+    { label: "Spanish", value: "es" },
+    { label: "Swahili", value: "sw" },
+    { label: "Swedish", value: "sv" },
+    { label: "Thai", value: "th" },
+    { label: "Turkish", value: "tr" },
+    { label: "Vietnamese", value: "vi" },
+]
+
+export const runTime = [
+    { label: "Up to 90 minute", value: "1" },
+    { label: "Between 90-150 minute", value: "2" },
+    { label: "Over 150 minute", value: "3" },
+]
+
+export const age = [
+    { label: "For kids", value: "kid" },
+    { label: "Mature (+18)", value: "adult" },
+]
