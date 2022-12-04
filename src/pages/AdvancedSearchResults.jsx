93209c29a929acdef50ec9635a782bb75496bf34
DoukanT <dogukanturgut@eskisehir.edu.tr>
2022-12-04 21:28:37 +0300

    Advanced search utility

--- src/pages/AdvancedSearchResults.jsx
+++ src/pages/AdvancedSearchResults.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
 import { useLocation } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Movie from '../components/Movie';
 
 
 const AdvancedSearchResults = () => {
   const data = useLocation();
   const url = data.state.url;
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setMovies(response.data.results);
+    });
+  }, [url]);
+
   return (
     <div className='w-100% h-auto ml-3'>
-      <h2 className='pt-28 text-white font-bold md:text-xl p-4'>Search results for '{url}'</h2>
-      
+      <h2 className='pt-28 text-white font-bold md:text-xl p-4'>{url}</h2>
+      <div className='flex flex-wrap scroll-smooth scrollbar-hide relative'>
+      {movies.map((item, id) => (
+        <Movie key={id} item={item} />
+      ))}
+      </div>
     </div>
   )
 }
