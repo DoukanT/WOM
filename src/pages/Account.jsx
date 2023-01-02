f987f5154d348f54a4b6a8c1ca0175af52655e38
serkankonuk <115952749+serkankonuk@users.noreply.github.com>
2023-01-02 11:18:05 +0300

    Revert "Merge branch 'deneme' of https://github.com/DoukanT/WOM into deneme"
    This reverts commit 2cfa8fdc4afc9e9fde4afefdcdbc5b2fe5af628f, reversing
    changes made to c950e4137bbac3f5338cf5fe9a32a7821a8a78a6.

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -6,7 +6,7 @@ const Account = () => {
   return (
     <>
       <div className='w-full text-black'>
-        <div className='pt-24'>
+        <div className='absolute pt-24'>
         <h1 className='text-white text-3xl font-bold p-6'>My List</h1>
         <SavedShows />
         <UnlikedShows />
